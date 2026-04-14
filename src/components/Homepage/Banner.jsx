fbe36907e280fef594efec2729201db800fa959e
Saha-Ovi <ovisaha3210@gmail.com>
2026-04-14 15:21:59 +0600

    banner section added

--- src/components/Homepage/Banner.jsx
+++ src/components/Homepage/Banner.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto my-8 space-y-4 text-center'>
+            <h2 className='text-4xl font-extrabold'>Friends to keep close in your life</h2>
+            <p className='text-sm'>Your personal shelf of meaningful connections. Browse, tend, and nurture the
+                relationships that matter most.</p>
+            <button className='btn bg-[#244D3F] text-white'><FiPlus />Add a Friend</button>
         </div>
     );
 };
