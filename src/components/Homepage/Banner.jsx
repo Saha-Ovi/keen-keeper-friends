2b342a853f7c4b4ce67436e0626898331b22e8c3
Saha-Ovi <ovisaha3210@gmail.com>
2026-04-15 01:53:16 +0600

    stats section added

--- src/components/Homepage/Banner.jsx
+++ src/components/Homepage/Banner.jsx
@@ -3,8 +3,8 @@ import { FiPlus } from 'react-icons/fi';
 
 const Banner = () => {
     return (
-        <div className='container mx-auto my-8 space-y-4 text-center'>
-            <h2 className='text-4xl font-extrabold'>Friends to keep close in your life</h2>
+        <div className='container mx-auto my-8 space-y-4 text-center p-4 lg:p-0'>
+            <h2 className='text-2xl md:text-4xl font-extrabold'>Friends to keep close in your life</h2>
             <p className='text-sm'>Your personal shelf of meaningful connections. Browse, tend, and nurture the
                 relationships that matter most.</p>
             <button className='btn bg-[#244D3F] text-white'><FiPlus />Add a Friend</button>
--- src/components/Homepage/Stats.jsx
+++ src/components/Homepage/Stats.jsx
@@ -2,8 +2,29 @@ import React from 'react';
 
 const Stats = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto my-10'>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-4 p-6 lg:p-0'>
+                <div className='shadow-xl text-center px-2 md:px-4 py-6 md:py-10 rounded-2xl'>
+                    <h3 className='text-2xl md:text-4xl font-bold'>10</h3>
+                    <p className='text-lg font-semibold '>Total Friends</p>
+                
+                </div>
+                <div className='shadow-xl text-center px-4 py-10 rounded-2xl'>
+                    <h3 className='text-2xl md:text-4xl font-bold'>3</h3>
+                    <p className='text-lg font-semibold '>On Track</p>
+                
+                </div>
+                <div className='shadow-xl text-center px-4 py-10 rounded-2xl'>
+                    <h3 className='text-2xl md:text-4xl font-bold'>6</h3>
+                    <p className='text-lg font-semibold '>Need Attention</p>
+                
+                </div>
+                <div className='shadow-xl text-center px-4 py-10 rounded-2xl'>
+                    <h3 className='text-2xl md:text-4xl font-bold'>12</h3>
+                    <p className='text-lg font-semibold '>Interaction This Month</p>
+                
+                </div>
+            </div>
         </div>
     );
 };
