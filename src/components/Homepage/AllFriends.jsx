33c850a23df77128536b033da213e2af96ca8ded
Saha-Ovi <ovisaha3210@gmail.com>
2026-04-17 19:57:54 +0600

    responsive in homepage and timeline done

--- src/components/Homepage/AllFriends.jsx
+++ src/components/Homepage/AllFriends.jsx
@@ -6,7 +6,7 @@ const AllFriends = ({friendsPromise}) => {
     // console.log(friends);
     return (
         <div className='container mx-auto my-8 space-y-6'>
-            <h2 className='font-bold text-2xl lg:text-4xl'>Your Friends</h2>
+            <h2 className='font-bold text-2xl lg:text-4xl p-3 '>Your Friends</h2>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-3 lg:gap-6'>
                 {
                     friends.map(friend=><Card key={friend.id} friend={friend}></Card>)
