ae5ceb409b00b0000d4da408cd79038a69a9aa74
Saha-Ovi <ovisaha3210@gmail.com>
2026-04-17 14:25:31 +0600

    react toastify added

--- src/context/ActionProvider.jsx
+++ src/context/ActionProvider.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { ActionContext } from './ActionContext';
+import { toast } from 'react-toastify';
 
 const ActionProvider = ({children}) => {
     const [activeAction,setActiveAction]=useState([]);
 
     const handleAction=({card,type})=>
     {
+        toast.success(`${type} history of ${card.name} is added to the TimeLine`);
         setActiveAction([...activeAction,{card:card,type:type}]);
     }
     console.log(activeAction);
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { RouterProvider } from 'react-router'
 
 import { router } from './routes/Router.jsx'
 import ActionProvider from './context/ActionProvider.jsx'
+import { Bounce, ToastContainer } from 'react-toastify'
 
 
 
@@ -13,6 +14,19 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ActionProvider>
       <RouterProvider router={router}></RouterProvider>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </ActionProvider>
   </StrictMode>,
 )
